e1531cd62f1af7657ccbac1b76a6de57fd3e736e
sifato_o <85351973+sifato@users.noreply.github.com>
2023-10-21 14:39:12 +0800

    first commit

---
+++ bookstore/footer.php
@@ -0,0 +1,11 @@
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2023 E-book Shop | Powered by <a href="https://www.facebook.com/sifato.o2001/">sifato_O</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
